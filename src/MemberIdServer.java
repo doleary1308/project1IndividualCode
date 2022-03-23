e2f0ba5722f1eb5a4bf9eee57a35106094914709
EricScheeler <ericscheeler@yahoo.com>
2022-03-22 21:25:18 -0500

    final prof code

--- src/MemberIdServer.java
+++ src/MemberIdServer.java
@@ -1,50 +1,50 @@
 import java.io.*;
 public class MemberIdServer implements Serializable {
-    private  int idCounter;
-    private static MemberIdServer server;
-    private MemberIdServer() {
-        idCounter = 1;
+  private  int idCounter;
+  private static MemberIdServer server;
+  private MemberIdServer() {
+    idCounter = 1;
+  }
+  public static MemberIdServer instance() {
+    if (server == null) {
+      return (server = new MemberIdServer());
+    } else {
+      return server;
     }
-    public static MemberIdServer instance() {
-        if (server == null) {
-            return (server = new MemberIdServer());
-        } else {
-            return server;
-        }
+  }
+  public int getId() {
+    return idCounter++;
+  }
+  public String toString() {
+    return ("IdServer" + idCounter);
+  }
+  public static void retrieve(ObjectInputStream input) {
+    try {
+      server = (MemberIdServer) input.readObject();
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
+    } catch(Exception cnfe) {
+      cnfe.printStackTrace();
     }
-    public int getId() {
-        return idCounter++;
+  }
+  private void writeObject(java.io.ObjectOutputStream output) throws IOException {
+    try {
+      output.defaultWriteObject();
+      output.writeObject(server);
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
     }
-    public String toString() {
-        return ("IdServer" + idCounter);
+  }
+  private void readObject(java.io.ObjectInputStream input) throws IOException, ClassNotFoundException {
+    try {
+      input.defaultReadObject();
+      if (server == null) {
+        server = (MemberIdServer) input.readObject();
+      } else {
+        input.readObject();
+      }
+    } catch(IOException ioe) {
+      ioe.printStackTrace();
     }
-    public static void retrieve(ObjectInputStream input) {
-        try {
-            server = (MemberIdServer) input.readObject();
-        } catch(IOException ioe) {
-            ioe.printStackTrace();
-        } catch(Exception cnfe) {
-            cnfe.printStackTrace();
-        }
-    }
-    private void writeObject(java.io.ObjectOutputStream output) throws IOException {
-        try {
-            output.defaultWriteObject();
-            output.writeObject(server);
-        } catch(IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
-    private void readObject(java.io.ObjectInputStream input) throws IOException, ClassNotFoundException {
-        try {
-            input.defaultReadObject();
-            if (server == null) {
-                server = (MemberIdServer) input.readObject();
-            } else {
-                input.readObject();
-            }
-        } catch(IOException ioe) {
-            ioe.printStackTrace();
-        }
-    }
+  }
 }
